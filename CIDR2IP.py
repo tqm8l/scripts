5f818a16605759cf1c666fb8bddbbdaf656d4081
tqm8l <tompealin@gmail.com>
2022-09-15 16:39:41 +0100

    Update CIDR2IP.py

--- CIDR2IP.py
+++ CIDR2IP.py
@@ -2,11 +2,9 @@ import sys
 import ipaddress
 from termcolor import colored
 print("Welcome to CIDR2IP")
-
 try:
 	cidr = str(sys.argv[1])
 	for ip in ipaddress.IPv4Network(cidr):
-		newip = str(ip)
-		print(colored(newip,'green'))
+		print(colored(str(ip),'green'))
 except Exception as e:
 	print(colored("broke",'red'))
